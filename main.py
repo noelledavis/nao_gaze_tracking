aee8af014a657f42072605ea10353c3be2ff9f38
Noelle <noelle798@users.noreply.github.com>
2015-07-30 14:13:39 -0500

    Updated main.py to account for changes in gaze.py

--- main.py
+++ main.py
@@ -4,48 +4,23 @@ import time
 import math
 import random
 
-from naoqi import ALProxy
-from naoqi import ALBroker
-
 import robot
 from gaze import Gaze
-from confidence import Confidence
 
 robot.connect()
-
-# create proxies
-motion = ALProxy("ALMotion")
-face_tracker = ALProxy("ALFaceTracker")
-posture = ALProxy("ALRobotPosture")
 
 # set game time limit
 max_game_time = 15
 
-object_yaws = []
-object_pitches = []
-
-object_angle_file = open("object_angles.txt")
-for object_angle in object_angle_file:
-	yaw, pitch = object_angle.split(', ')
-	object_yaws.append(float(yaw))
-	object_pitches.append(float(pitch))
-
-# set object angles, error, and confidences
-object_angle_error = math.radians(15)
-confidences = Confidence(object_yaws, object_angle_error)
-
 # get into starting position (sitting down, looking up towards person)
-motion.setStiffnesses("Body", 1.0)
-posture.goToPosture("Crouch", 0.2)
-motion.setAngles("HeadPitch", math.radians(-10), 0.2)
+robot.robot().wake()
+robot.robot().turnHead(pitch = math.radians(-10))
 
 # give robot some time to get to this angle before starting face tracker
 time.sleep(0.5)
 
 # start face tracker
-face_tracker.setWholeBodyOn(False)
-face_tracker.startTracker()
-print 'Face tracker successfully started!'
+robot.robot().trackFace()
 
 # wait a little to let robot find face
 time.sleep(0.5)
@@ -57,28 +32,15 @@ gaze.findPersonPitchAdjustment()
 # set timer
 timeout = time.time() + max_game_time
 
-# while 'q' is not pressed and time limit isn't reached
+# while the time limit isn't reached
 while time.time() < timeout:
 
-	gaze_location = gaze.getObjectLocation()
-	
-	# if can't get location of person's gaze or if it's not near the objects
-	if gaze_location is None:
-		continue
-
-	confidences.update(gaze.robot_object_yaw)
-
-gaze.stop()
+	gaze.track()
 
 # stop face tracker
-face_tracker.stopTracker()
-print "Face tracker stopped."
+robot.robot().stopTrackingFace()
 
-confidences.normalize()
-confidences.guess()
+gaze.analyze()
 
 # sit down slowly
-posture.goToPosture("Crouch", 0.2)
-
-# remove stiffness
-motion.setStiffnesses("Body", 0.0)
+robot.robot().rest()
